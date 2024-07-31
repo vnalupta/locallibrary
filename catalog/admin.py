009eef2485dd24692e062f1c29778e6ffcd1524e
Vincent Nalupta <vnalupta.tech@gmail.com>
2024-07-30 22:50:41 -0400

    views added

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Author, Genre, Book, BookInstance, Language
+
+#admin.site.register(Author)
+#admin.site.register(Book)
+#admin.site.register(BookInstance)
+admin.site.register(Genre)
+admin.site.register(Language)
+
+# Inline view for Books
+class BookInline(admin.TabularInline):
+    model = Book
+    extra = 0
+
+# Define the admin class
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    inlines = [BookInline]
+
+# Register the admin class with the associated model
+# admin.site.register(Author, AuthorAdmin)
+
+# Inline view for Books
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
+# Register the Admin classes for Book using the decorator
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+
+# Register the Admin classes for BookInstance using the decorator
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_back', 'id')
+    list_filter = ('status', 'due_back')
